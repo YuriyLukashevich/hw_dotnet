eec5baa425ac1988ae85ea33c078624a83e99bba
Юрий Лукашевич <yu.lukashevich@mail.ru>
2019-09-29 12:20:24 +0300

    task1 and task2 in hm4

---
+++ ItAcademyHomeWorks/hm_04_Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hm_04_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numberLetter = 122; numberLetter>=97; numberLetter--)
+            {
+                Console.WriteLine(Convert.ToChar(numberLetter));
+            }
+        }
+    }
+}
---
+++ ItAcademyHomeWorks/hm_04_Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hm_04_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите направление движения: вверх(w), вниз(s), влево(a) или вправо(d)");
+            String way = Console.ReadLine();
+        }
+    }
+}
