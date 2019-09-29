b26d7a44c4c4b2dbfe086c7c1a9c4023cdbdb442
Юрий Лукашевич <yu.lukashevich@mail.ru>
2019-09-29 16:18:25 +0300

    done Task3 from hm4

---
+++ ItAcademyHomeWorks/hm_04_Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hm_04_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deposit newDeposit = new Deposit();
+            Console.WriteLine("Введите сумму вклада: ");
+            newDeposit.deposit = Convert.ToDouble(Console.ReadLine());
+            newDeposit.IncreaseInAmount();
+            newDeposit.DepositAmount();
+
+        }
+    }
+}
