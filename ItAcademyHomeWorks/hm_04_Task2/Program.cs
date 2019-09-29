06d598f640c68696ef6c112035618d7ab957a6f6
Юрий Лукашевич <yu.lukashevich@mail.ru>
2019-09-29 14:04:35 +0300

    done task2 from hm_04

--- ItAcademyHomeWorks/hm_04_Task2/Program.cs
+++ ItAcademyHomeWorks/hm_04_Task2/Program.cs
@@ -6,8 +6,31 @@ namespace hm_04_Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите направление движения: вверх(w), вниз(s), влево(a) или вправо(d)");
-            String way = Console.ReadLine();
+            string variant;
+            Way way1 = new Way();
+            Console.WriteLine("Фигуру необходимо переместить? напишите \"да\" или \"нет\" ");
+            do
+            {                
+                variant = Console.ReadLine();
+                switch (variant)
+                {
+                    case "да":
+                        variant = "default";
+                        way1.ToWay();
+                        Console.WriteLine("Фигуру еще необходимо перемещать? напишите \"да\" или \"нет\" ");
+                        break;
+                    case "нет":
+                        Console.WriteLine("Фигуру никуда перемещать не нужно");
+                        break;
+                    default:
+                        variant = "default";
+                        Console.WriteLine("Вы ввели неправильное значение, введите \"да\" или \"нет\" ");
+                        break;
+                }
+            }
+            while (variant=="default");
+            
+            
         }
     }
 }
---
+++ ItAcademyHomeWorks/hm_04_Task2/Way.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_04_Task2
+{
+    class Way
+    {
+        public string way;
+        public int wayFromUnicode;
+        public void ToWay()
+        {
+            Console.WriteLine("Введите направление движения: вверх(w), вниз(s), влево(a) или вправо(d)");
+            do
+            {
+                way = Console.ReadLine();
+                wayFromUnicode = Convert.ToChar(way);
+                switch (wayFromUnicode)
+                {
+                    case 119:
+                        Console.WriteLine("двигаем фигуру вверх");
+                        break;
+                    case 97:
+                        Console.WriteLine("двигаем фигуру влево");
+                        break;
+                    case 115:
+                        Console.WriteLine("двигаем фигуру ввниз");
+                        break;
+                    case 100:
+                        Console.WriteLine("двигаем фигуру вправо");
+                        break;
+                    default:
+                        way = "default";
+                        Console.WriteLine("Вы ввели неправильно значение направление, введите w, a, s или d");
+                        break;
+                }
+            }
+            while (way == "default");
+        }
+    }
+}
