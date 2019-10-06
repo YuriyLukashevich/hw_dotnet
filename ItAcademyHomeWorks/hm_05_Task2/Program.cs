ee72f77230fca8bb4720d213772f94d703ff4847
Юрий Лукашевич <yu.lukashevich@mail.ru>
2019-10-06 13:42:46 +0300

    Add HM_05

---
+++ ItAcademyHomeWorks/hm_05_Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace hm_05_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mass();
+        }
+
+        static void Mass()
+        {
+            int[] mass = new int[10];
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                Console.Write($"Insert {i + 1} array element: ");
+                if (Int32.TryParse(Console.ReadLine(), out mass[i])) ;
+                else
+                    Console.WriteLine("incorrect input");
+            }
+            Console.WriteLine($"mass: {string.Join(", ", mass)}");
+            Console.Write("Insert new array element: ");
+            try
+            {
+                int newElement = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Insert position of new array element, enter from 1 to 10:  ");
+                int position = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int i in mass)
+                {
+                    if (position == i)
+                    {
+                        if (position == mass.Length)
+                        {
+                            mass[mass.Length] = newElement;
+                        }
+                        else
+                        {
+                            for (int j = mass.Length - 1; j >= position; j--)
+                            {
+                                if (j == position)
+                                    mass[position] = newElement;
+                                else
+                                    mass[j] = mass[j - 1];
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"changed mass: {string.Join(", ", mass)}");
+            }
+            catch
+            {
+                Console.WriteLine("incorrect input");
+            }
+        }
+    }
+}
---
+++ ItAcademyHomeWorks/hm_05_Task3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace hm_05_Task3
+{
+    class Program
+    {
+        public static void Way1()
+        {
+            object[] array1 = { "object", 123, 'o', 4 };
+            Console.WriteLine($"before reverse: {String.Join(", ", array1)}");
+            Array.Reverse(array1);
+            Console.WriteLine($"after reverse: {String.Join(", ", array1)}");
+        }
+
+        public static void Way2()
+        {
+            object[] array1 = new object[10];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write($"obj {i + 1} = ");
+                array1[i] = Console.ReadLine();
+            }
+            Console.WriteLine($"before reverse: {String.Join(", ", array1)}");
+            Array.Reverse(array1);
+            Console.WriteLine($"after reverse: {String.Join(", ", array1)}");
+        }
+
+        public static void Way3()
+        {
+            Random fill = new Random();
+            int[] array1 = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                array1[i] = fill.Next(0, 100);
+                Console.WriteLine($"{i + 1} element of mass1: {array1[i]}");
+            }
+            Console.WriteLine($"before reverse: {String.Join(", ", array1)}");
+            Array.Reverse(array1);
+            Console.WriteLine($"after reverse: {String.Join(", ", array1)}");
+
+        }
+
+        public static void Way4()
+        {
+            try
+            {
+                object[] array1 = null;
+                Console.WriteLine($"before reverse: {String.Join(", ", array1)}");
+                Array.Reverse(array1);
+                Console.WriteLine($"after reverse: {String.Join(", ", array1)}");
+            }
+            catch
+            {
+                Console.WriteLine("incorrect input");
+            }
+        }
+
+        public static void Way()
+        {
+            int option;
+            Console.WriteLine("Выберите вариант какого массива произвести реверс: ");
+            Console.WriteLine("1) Уже заполненный. ");
+            Console.WriteLine("2) Заполненный самостоятельно. ");
+            Console.WriteLine("3) Заполненный рандомно. ");
+            Console.WriteLine("4) Нулевой массив. ");
+            if (Int32.TryParse(Console.ReadLine(), out option))
+                switch (option)
+                {
+                    case 1:
+                        Way1();
+                        break;
+                    case 2:
+                        Way2();
+                        break;
+                    case 3:
+                        Way3();
+                        break;
+                    case 4:
+                        Way4();
+                        break;
+                    default:
+                        Console.WriteLine("incorrect input");
+                        break;
+                }
+            else
+                Console.WriteLine("incorrect input");
+        }
+
+        static void Main(string[] args)
+        {
+            Way();
+        }
+    }
+}
