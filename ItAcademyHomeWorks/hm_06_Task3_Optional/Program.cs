1793503e58c360c1455730e33a7e60237618302a
Юрий Лукашевич <yu.lukashevich@mail.ru>
2019-10-08 16:36:47 +0300

    add hm_06

---
+++ ItAcademyHomeWorks/hm_06_Task3_Optional/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hm_06_Task3_Optional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //with boxing
+            int years = 18;
+            string old1 = "I am ";
+            string old3 = old1 + years + ".";
+            Console.WriteLine(old3);
+            //without boxing
+            string old2 = "I am " + years.ToString() + ".";
+            Console.WriteLine(old2);
+        }
+    }
+}
