300d263ea6fcecb56804359955ca4e7892719ad2
Юрий Лукашевич <yu.lukashevich@mail.ru>
2019-10-20 22:36:14 +0300

    add hm_09

---
+++ ItAcademyHomeWorks/hm_09_task1/MotorcycleRepos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hm_09_task1
+{
+    class MotorcycleRepos
+    {        
+        public static List<Motorcycle> motorcycles = new List<Motorcycle>();
+        private static int _id = 1;
+
+        public void GetMotorcycle()
+        {
+            foreach (var motoModel in motorcycles)
+            {
+                Console.WriteLine($"{motoModel.Id}) Название мотоцикла: {motoModel.Name};\n" +
+                    $"Год производства: {motoModel.Year};\n" +
+                    $"Максмальная скорость: {motoModel.MaxSpeed};\n" +
+                    $"Вин номер: {motoModel.VinNumber}.");
+            }
+            Console.WriteLine();
+            Logger.Log.Info("Вывод всех мотоциклов");
+        }
+
+        public void AddMotorcycle(Motorcycle motorcycle)
+        {
+            motorcycle.Id = _id;
+            _id++;
+            motorcycles.Add(motorcycle);
+            Logger.Log.Info($"Добавление нового мотоцикла: {motorcycle.Name}");
+        }
+
+        public Motorcycle UpdateMotorcycle(Motorcycle motorcycle)
+        {
+            var moto = motorcycles.SingleOrDefault(id => id.Id == motorcycle.Id);
+            Logger.Log.Info($"Обновление мотоцикла №{motorcycle.Id}");
+            moto.Name = motorcycle.Name;
+            moto.Year = motorcycle.Year;
+            moto.MaxSpeed = motorcycle.MaxSpeed;
+            moto.VinNumber = motorcycle.VinNumber;
+            return moto;
+        }
+
+        public void GetMotorcycleByID(int id)
+        {
+            var moto = motorcycles.SingleOrDefault(item => item.Id == id);
+            Console.WriteLine($"{moto.Id}) Название мотоцикла: {moto.Name};\n" +
+                    $"Год производства: {moto.Year};\n" +
+                    $"Максмальная скорость: {moto.MaxSpeed};\n" +
+                    $"Вин номер: {moto.VinNumber}.\n");
+            Logger.Log.Info($"GetMotorcycleByID №{moto.Id}");
+        }
+
+        public void DeleteMotorcycle(int id)
+        {
+            var moto = motorcycles.SingleOrDefault(item => item.Id == id);
+            motorcycles.Remove(moto);
+            Logger.Log.Info($"Удаление мотоцикла №{moto.Id}: {moto.Name}");
+        }
+    }
+}
---
+++ ItAcademyHomeWorks/hm_09_task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm_09_task1
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Logger.InitLogger();
+            MotorcycleRepos motorcycleRepos = new MotorcycleRepos();
+            Motorcycle moto = new Motorcycle("Java", 1985, 120, "TLJ 638");
+            Motorcycle moto1 = new Motorcycle("Suzuki", 1999, 250, "JS1VP53A3");
+            motorcycleRepos.AddMotorcycle(moto);
+            motorcycleRepos.AddMotorcycle(moto1);
+            motorcycleRepos.GetMotorcycle();
+            //get by id
+            motorcycleRepos.GetMotorcycleByID(1);
+            //update
+            moto1.Name = "BMW";
+            moto1.VinNumber = "WBABT31060L";
+            motorcycleRepos.UpdateMotorcycle(moto1);
+            motorcycleRepos.GetMotorcycle();
+            //delete by id
+            motorcycleRepos.DeleteMotorcycle(2);
+            motorcycleRepos.GetMotorcycle();
+            Console.ReadKey();
+        }
+    }
+}
